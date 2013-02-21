111f375f53a69495c1e1f474997ad2d336c5f1db
Chris <chris@buryta.com>
2013-02-20 22:48:01 -0500

    clean up spacing, use execute blocks to take advantage of creates call to avoid duplicate downlaods

---
+++ CHANGELOG.md
@@ -0,0 +1,4 @@
+0.0.2
+=====
+
+- use executes not bash, enables use of the creates flag to avoid multiple downloads of the same file
--- recipes/default.rb
+++ recipes/default.rb
@@ -9,29 +9,32 @@ include_recipe "php"
 
 #install/upgrade curl
 package "curl" do
-	action :upgrade
+    action :upgrade
 end
 
-command = "curl -s https://getcomposer.org/installer | php"
+# base install command
+install_command = "curl -s https://getcomposer.org/installer | php"
 
-unless node[:composer][:install_globally]
-	unless node[:composer][:install_dir].nil? || node[:composer][:install_dir].empty?
-		command = command + " -- --install-dir=#{node[:composer][:install_dir]}"
+# only if installing locally do we add the flag directing where to install it
+unless node['composer']['install_globally']
+    unless node['composer']['install_dir'].nil? || node['composer']['install_dir'].empty?
+        install_command = install_command + " -- --install-dir=#{node['composer']['install_dir']}"
     end
 end
 
-bash "dowload_composer" do
-	cwd "#{Chef::Config[:file_cache_path]}"
-	code <<-EOH
-		#{command}
-	EOH
+# if not yet installed, download the phar
+# if installing locally, we're done, the above code will define the final install-dir
+bash 'dowload_composer' do
+    creates "#{node['composer']['prefix']}/bin/composer"
+    cwd Chef::Config['file_cache_path']
+    command install_command
 end
 
-if node[:composer][:install_globally]
-	bash "move_composer" do
-		cwd "#{Chef::Config[:file_cache_path]}"
-		code <<-EOH
-			sudo mv composer.phar #{node[:composer][:prefix]}/bin/composer
-		EOH
-	end
+# when installing globally, we move it to a global spot after install
+if node['composer']['install_globally']
+    execute 'move_composer' do
+        creates "#{node['composer']['prefix']}/bin/composer"
+        cwd Chef::Config["file_cache_path"]
+        command "mv composer.phar #{node['composer']['prefix']}/bin/composer"
+    end
 end
