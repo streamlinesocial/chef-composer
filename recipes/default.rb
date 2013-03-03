76a2a27b467964910d1b583b74ae68a705e78bd4
Chris Buryta <chris@buryta.com>
2013-03-02 22:54:54 -0500

    Fix condition of the install, was getting issues.

--- recipes/default.rb
+++ recipes/default.rb
@@ -14,27 +14,30 @@ end
 
 # base install command
 install_command = "curl -s https://getcomposer.org/installer | php"
+install_destination = "#{node['composer']['prefix']}/bin/composer"
 
 # only if installing locally do we add the flag directing where to install it
 unless node['composer']['install_globally']
     unless node['composer']['install_dir'].nil? || node['composer']['install_dir'].empty?
+        install_destination "#{node['composer']['install_dir']}/composer.phar"
         install_command = install_command + " -- --install-dir=#{node['composer']['install_dir']}"
     end
 end
 
 # if not yet installed, download the phar
 # if installing locally, we're done, the above code will define the final install-dir
-bash 'dowload_composer' do
-    creates "#{node['composer']['prefix']}/bin/composer"
+execute install_command do
+    not_if "test -f #{install_destination}"
     cwd Chef::Config['file_cache_path']
     command install_command
+    if node['composer']['install_globally']
+        notifies :run, "execute[move_composer]"
+    end
 end
 
 # when installing globally, we move it to a global spot after install
-if node['composer']['install_globally']
-    execute 'move_composer' do
-        creates "#{node['composer']['prefix']}/bin/composer"
-        cwd Chef::Config["file_cache_path"]
-        command "mv composer.phar #{node['composer']['prefix']}/bin/composer"
-    end
+execute 'move_composer' do
+    action :nothing
+    cwd Chef::Config["file_cache_path"]
+    command "mv composer.phar #{install_destination}"
 end
